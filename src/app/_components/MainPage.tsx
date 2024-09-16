be4dc52a09fbb01695a0dc2181eeebeb6326f6ee
Panu Tanavatavivat <markpanya@gmail.com>
2024-09-17 03:35:16 +0700

    feat: create todo

--- src/app/_components/MainPage.tsx
+++ src/app/_components/MainPage.tsx
@@ -1,8 +1,11 @@
 'use client'
 
+import NavBar from '@/app/_components/NavBar'
+
 export default function MainPage() {
   return (
     <div className="bg-white">
+      <NavBar />
       <h1>Marwin</h1>
       <span>Push dev</span>
     </div>
