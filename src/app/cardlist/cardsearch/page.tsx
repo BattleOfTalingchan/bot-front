83076455ffb6634aca603d4f527b3f28fb123e1b
SarZee <thorntirawit4800@gmail.com>
2024-09-18 03:53:41 +0700

    Revert "Card Detail"

--- src/app/cardlist/cardsearch/page.tsx
+++ src/app/cardlist/cardsearch/page.tsx
@@ -1,9 +1,7 @@
-import CardDetail from "./_components/CardDetail"
-
 export const dynamic = 'force-dynamic'
 
 export default function CardSearchPage() {
   return (
-    <CardDetail/>
+    <div>CardSearchPage</div>
   )
 }
