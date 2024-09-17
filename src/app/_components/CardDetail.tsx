6b7d57818d845bf20ff6a1d73a3465acef7140f2
Mavintino <93636601+Mavintino@users.noreply.github.com>
2024-09-18 02:29:10 +0700

    not use this

--- src/app/_components/CardDetail.tsx
+++ src/app/_components/CardDetail.tsx
@@ -9,10 +9,10 @@ function CardDetail() {
                 </div>
             </div>
             <div className = "flex">
-                <img src="https://www.konami.com/kde_cms/na_publish/uploads/slifer-the-sky-dragon.png" alt=""  className ="w-64 ml-96 mt-28"/>
+                <img src="https://cdn.discordapp.com/attachments/626262617631227923/1285670645828419604/image.png?ex=66eb1d98&is=66e9cc18&hm=0bf5ab1ade7ac61719d48329bdf125cdd0afb84271cdcb5b028c428699fd363d&" alt=""  className ="w-64 ml-96 mt-28"/>
                 <div className="w-[600px] h-[100px] p-[50px] mt-16 ml-10">
                     <span className = "text-black text-3xl font-bold">
-                        Osiris
+                        พระอิศวร
                     </span>
                     <p className="text-justify text-black">
                         Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
