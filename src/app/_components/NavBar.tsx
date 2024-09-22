82613f170b0e1d2ba9bb3eb99b5bc0d823b26234
SarZee <thorntirawit4800@gmail.com>
2024-09-22 18:12:26 +0700

    Fix Navbar display

--- src/app/_components/NavBar.tsx
+++ src/app/_components/NavBar.tsx
@@ -7,7 +7,7 @@ export default function NavBar() {
         <div className="">
           <a href="/">
             <img
-              src="https://cdn.discordapp.com/attachments/1285740112868806657/1285740186453409863/logo.png?ex=66eb5e5c&is=66ea0cdc&hm=700889531d6e020e5af92e685bf291b530e3732b28af6cecec72130838eecc7f&"
+              src="https://cdn.discordapp.com/attachments/1285740112868806657/1285740186453409863/logo.png?ex=66f0a45c&is=66ef52dc&hm=73d9c5a9d7f2d7ee9d7020bd0c97c5bfa051a0e521692b36f588519580819fc4&"
               alt="Logo"
               className="w-[70px]"
             />
@@ -21,14 +21,12 @@ export default function NavBar() {
             <a href="" className="content-center">
               กฎกติกา
             </a>
-            <span className="flex gap-3">
-              <form action="/cardlist/cardsearch" method="post">
-                <input type="text" className="w-[180px] px-2 text-black" placeholder="ค้นหา" />
-                <div className="h-[40px] w-[40px] bg-zinc-900 p-[5px]">
-                  <button>
-                    <Search />
-                  </button>
-                </div>
+            <span className="flex content-center">
+              <form className="flex gap-3" action="/cardlist/cardsearch" method="post">
+                <input type="text" className="flex w-[180px] px-2 text-black" placeholder="ค้นหา" />
+                <button className="flex h-[40px] w-[40px] bg-zinc-900 p-[5px]">
+                  <Search />
+                </button>
               </form>
             </span>
           </div>
