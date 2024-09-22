c31b3375f21e524576697801bfe1bbdacf8bb47d
pumipattitle <pumipattitle@gmail.com>
2024-09-23 00:53:53 +0700

    commit undone code

--- src/app/cardlist/cardsearch/_components/List.tsx
+++ src/app/cardlist/cardsearch/_components/List.tsx
@@ -1,1 +1,36 @@
-skjfkajsfbjn
+export default function List() {
+    return (
+    <div className="bg-black">
+      <div className="text-white text-6xl text-center pt-10 ">Cardlist</div>
+      <div className="flex justify-between">
+        <div className="text-blue-400 text-6xl pl-20 flex">Title</div>
+        <div className="text-blue-400 text-6xl pr-20 flex">Pumipat</div>
+      </div>
+      <div className="flex pl-20 text-6xl text-red-700 pt-10">Prammatas</div>
+      <div className="flex justify-between pt-10">
+        <div className="text-blue-400 text-6xl pl-20 flex">Title</div>
+        <div className="text-blue-400 text-6xl pr-20 flex">Pumipat</div>
+      </div>
+      <div className="flex flex-col pt-10">
+          <div className="flex justify-center border border-red-600 items-center gap-24 cursor-pointer flex-wrap w-[2500px]">
+            <img src="https://media.discordapp.net/attachments/626262617631227923/1285670645828419604/image.png?ex=66f10c58&is=66efbad8&hm=5ea426939a3197b48f161b8265709f084df6199334ae00592be7fb38308526f0&=&format=webp&quality=lossless" alt="" />
+            <img src="https://media.discordapp.net/attachments/626262617631227923/1285670645828419604/image.png?ex=66f10c58&is=66efbad8&hm=5ea426939a3197b48f161b8265709f084df6199334ae00592be7fb38308526f0&=&format=webp&quality=lossless" alt="" />
+            <img src="https://media.discordapp.net/attachments/626262617631227923/1285670645828419604/image.png?ex=66f10c58&is=66efbad8&hm=5ea426939a3197b48f161b8265709f084df6199334ae00592be7fb38308526f0&=&format=webp&quality=lossless" alt="" />
+            <img src="https://media.discordapp.net/attachments/626262617631227923/1285670645828419604/image.png?ex=66f10c58&is=66efbad8&hm=5ea426939a3197b48f161b8265709f084df6199334ae00592be7fb38308526f0&=&format=webp&quality=lossless" alt="" />
+            <img src="https://media.discordapp.net/attachments/626262617631227923/1285670645828419604/image.png?ex=66f10c58&is=66efbad8&hm=5ea426939a3197b48f161b8265709f084df6199334ae00592be7fb38308526f0&=&format=webp&quality=lossless" alt="" />
+            <img src="https://media.discordapp.net/attachments/626262617631227923/1285670645828419604/image.png?ex=66f10c58&is=66efbad8&hm=5ea426939a3197b48f161b8265709f084df6199334ae00592be7fb38308526f0&=&format=webp&quality=lossless" alt="" />
+            <img src="https://media.discordapp.net/attachments/626262617631227923/1285670645828419604/image.png?ex=66f10c58&is=66efbad8&hm=5ea426939a3197b48f161b8265709f084df6199334ae00592be7fb38308526f0&=&format=webp&quality=lossless" alt="" />
+            <img src="https://media.discordapp.net/attachments/626262617631227923/1285670645828419604/image.png?ex=66f10c58&is=66efbad8&hm=5ea426939a3197b48f161b8265709f084df6199334ae00592be7fb38308526f0&=&format=webp&quality=lossless" alt="" />
+            <img src="https://media.discordapp.net/attachments/626262617631227923/1285670645828419604/image.png?ex=66f10c58&is=66efbad8&hm=5ea426939a3197b48f161b8265709f084df6199334ae00592be7fb38308526f0&=&format=webp&quality=lossless" alt="" />
+            <img src="https://media.discordapp.net/attachments/626262617631227923/1285670645828419604/image.png?ex=66f10c58&is=66efbad8&hm=5ea426939a3197b48f161b8265709f084df6199334ae00592be7fb38308526f0&=&format=webp&quality=lossless" alt="" />
+            <img src="https://media.discordapp.net/attachments/626262617631227923/1285670645828419604/image.png?ex=66f10c58&is=66efbad8&hm=5ea426939a3197b48f161b8265709f084df6199334ae00592be7fb38308526f0&=&format=webp&quality=lossless" alt="" />
+            <img src="https://media.discordapp.net/attachments/626262617631227923/1285670645828419604/image.png?ex=66f10c58&is=66efbad8&hm=5ea426939a3197b48f161b8265709f084df6199334ae00592be7fb38308526f0&=&format=webp&quality=lossless" alt="" />
+            <img src="https://media.discordapp.net/attachments/626262617631227923/1285670645828419604/image.png?ex=66f10c58&is=66efbad8&hm=5ea426939a3197b48f161b8265709f084df6199334ae00592be7fb38308526f0&=&format=webp&quality=lossless" alt="" />
+            <img src="https://media.discordapp.net/attachments/626262617631227923/1285670645828419604/image.png?ex=66f10c58&is=66efbad8&hm=5ea426939a3197b48f161b8265709f084df6199334ae00592be7fb38308526f0&=&format=webp&quality=lossless" alt="" />
+            <img src="https://media.discordapp.net/attachments/626262617631227923/1285670645828419604/image.png?ex=66f10c58&is=66efbad8&hm=5ea426939a3197b48f161b8265709f084df6199334ae00592be7fb38308526f0&=&format=webp&quality=lossless" alt="" />
+
+          </div>
+        </div>
+      </div>
+      )
+  }
--- src/app/cardlist/cardsearch/page.tsx
+++ src/app/cardlist/cardsearch/page.tsx
@@ -1,7 +1,10 @@
+import Card from "./_components/Card"
+import List from "./_components/List"
+
 export const dynamic = 'force-dynamic'
 
 export default function CardSearchPage() {
   return (
-    <div>CardSearchPage</div>
+    <List></List>
   )
 }
